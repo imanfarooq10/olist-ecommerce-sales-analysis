f2b8740598072a55191d9dac90a48121a45a13e4
imanfarooq10 <imanfarooq755@gmail.com>
2026-07-27 19:45:25 +0500

    late delivery(not completed)

--- sql/01_total_revenue_and_aov.sql
+++ sql/01_total_revenue_and_aov.sql
@@ -2,6 +2,5 @@ SELECT COUNT(DISTINCT orders.order_id) AS total_orders,
 ROUND(SUM(price),2) AS total_revenue,
 ROUND(SUM(price)/COUNT(DISTINCT orders.order_id),2) AS avg_order_revenue
 FROM orders
-JOIN order_items
-ON orders.order_id=order_items.order_id
+JOIN order_items ON orders.order_id=order_items.order_id
 WHERE order_status = 'delivered';
